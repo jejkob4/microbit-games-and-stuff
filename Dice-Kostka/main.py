f06666ce79425ebbc3f39f582fa56f129679356f
Jakub Krejčíř <128169549+jejkob4@users.noreply.github.com>
2023-04-01 16:18:29 +0200

    Create main.py

---
+++ Dice-Kostka/main.py
@@ -0,0 +1,50 @@
+from microbit import *
+import random
+# startovací sekvence
+
+jedna = (Image('00000:'
+           '00000:'
+           '00900:'
+           '00000:'
+           '00000'))
+
+dva = (Image('00000:'
+           '00090:'
+           '00000:'
+           '09000:'
+           '00000'))
+
+tri = (Image('00000:'
+           '00090:'
+           '00900:'
+           '09000:'
+           '00000'))
+
+ctyri = (Image('00000:'
+               '09090:'
+               '00000:'
+               '09090:'
+               '00000'))
+
+pet = (Image('00000:'
+             '09090:'
+             '00900:'
+             '09090:'
+             '00000'))
+
+sest = (Image('00000:'
+           '09090:'
+           '09090:'
+           '09090:'
+           '00000'))
+
+
+kocka = [jedna, dva, tri, ctyri, pet, sest]
+
+
+#pořád
+while True:
+    if button_a.was_pressed() or button_b.was_pressed() or pin_logo.is_touched():
+        x = random.choice(kocka)
+        display.show(x)
+        print(x)

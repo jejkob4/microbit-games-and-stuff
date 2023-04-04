8192daf30491e8beb204e9d99552086f8a582252
Jakub Krejčíř <128169549+jejkob4@users.noreply.github.com>
2023-04-04 18:53:07 +0200

    ss
    ss

---
+++ main.py
@@ -0,0 +1,7 @@
+#hra s názvem "Reakční časovač", kde hráč musí co nejrychleji stisknout tlačítko na micro:bitu poté, co se rozsvítí LED?
+
+#V této hře se micro:bit náhodně rozsvítí LED a hráč musí co nejrychleji stisknout tlačítko, aby ji vypnul. 
+#Micro:bit poté zobrazí reakční čas hráče. 
+#Hra by mohla mít několik úrovní, kde se LED rozsvítí na kratší dobu nebo kde bude hráč mít méně času na reakci, jak se bude postupovat do dalších úrovní.
+#Pro implementaci této hry by bylo potřeba připojit tlačítko a LED k micro:bitu a naprogramovat micro:bit tak, 
+#aby náhodně rozsvítil LED a detekoval stisk tlačítka k jejímu vypnutí. Také by bylo možné přidat zvukové efekty nebo animace LED, aby byla hra pro hráče zajímavější.
